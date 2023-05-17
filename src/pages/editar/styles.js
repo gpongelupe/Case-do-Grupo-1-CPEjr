bf0458683749a81475c49363f459c52565e09c5b
marco-stalin <marcosena@cpejr.com.br>
2023-05-17 16:53:01 -0300

    tentativa de colocar fundo na pagina editar

--- src/pages/editar/styles.js
+++ src/pages/editar/styles.js
@@ -1,4 +1,15 @@
 import styled from "styled-components";
+import fundoMinecraft from "../../assets/editar/fundoMinecraft.jpg"
+
+export const Container = styled.div`
+height: 100%;
+width: 100%;
+
+background-color: aqua;
+/* background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
+    url(${fundoMinecraft});
+  background-size: cover; */
+`;
 
 export const Title = styled.h1`
 font-size: 50px;
--- src/pages/home/styles.js
+++ src/pages/home/styles.js
@@ -3,3 +3,10 @@ import styled from "styled-components";
 export const Title = styled.h1`
 font-size: 50px;
 `;
+export const Container = styled.div`
+display:flex;
+flex-direction: column;
+background-color: azure;
+height: 100%;
+width:100%;
+`;

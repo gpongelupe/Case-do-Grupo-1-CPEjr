05b73f5699f2b1ab79f1dfd29c4f95ae9f389d72
marco-stalin <marcosena@cpejr.com.br>
2023-05-18 20:49:08 -0300

    Mais config da editar²

--- src/pages/editar/editar.jsx
+++ src/pages/editar/editar.jsx
@@ -1,24 +1,25 @@
 //import { text } from "stream/consumers";
-import {Container, Title, Texto, MeuBotao, Input} from "./styles"
+import {Container, Title, Texto, MeuBotao, Input, Organizar, Item} from "./styles"
 
 export default function Editar(){
    return(
    <Container>
       <Title>O que deseja editar?</Title>
-      <div>
-      <div>
-      <Texto>Nome: </Texto> <Input>
+      <Organizar>
+   <Item>
+      <Texto>Nome:</Texto> <Input placeholder=" Digite seu novo Nome">
          </Input><MeuBotao><Texto>Editar! </Texto></MeuBotao>
-      </div><br></br>
-      <div>
-        <Texto>Cargo: </Texto><Input>
+         </Item>
+         <Item>
+      <Texto>Cargo:</Texto> <Input placeholder=" Digite seu novo Cargo">
          </Input><MeuBotao><Texto>Editar! </Texto></MeuBotao>
-      </div><br></br>
-      <div>
-      <Texto>Status: </Texto><Input>
+         </Item>
+         <Item>
+      <Texto>Status:</Texto> <Input placeholder=" Digite seu novo Status">
          </Input><MeuBotao><Texto>Editar! </Texto></MeuBotao>
-      </div><br></br>
-   </div>
+         </Item>
+         </Organizar>
+
      </Container>
    );
 }
--- src/pages/editar/styles.js
+++ src/pages/editar/styles.js
@@ -2,41 +2,74 @@ import styled from "styled-components";
 import fundoMinecraft from "../../assets/editar/fundoMinecraft.jpg"
 
 export const Container = styled.div`
-position:fixed;
-    padding:0;
-    margin:0;
-    top:0;
-    left:0;
-    width: 100%;
-    height: 100%;
-    background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.5)),
-    url(${fundoMinecraft});
-    background-size: cover;
+//position:fixed;
+  padding:0;
+  margin:0;
+  top:100px;
+  left:0;
+  width: 100%;
+  height: 100%;
+  background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.5)),
+  url(${fundoMinecraft});
+  background-size: cover;  
 `;
 export const Title = styled.p`
-  font-size: 50px;
   font-family: "minecraft";
   color: #FFFFFF;
   text-align: center;
+  margin-bottom: 170px;
+  margin-top: 50px;
+  font-size: 60px;
+  font-weight: 500;
+  line-height: 65px;
+  letter-spacing: 0.075em;
 `;
 export const Texto = styled.span` 
   font-size: 30px;
   font-family: "minecraft";
   color: #FFFFFF;
   text-align: left;
+  margin-top: 15px;
+  margin-right: 10px;
 `;
 export const MeuBotao = styled.button`
   background: #6D9948;
-  border-color: #6D9948;
+  border: none;
   width:215px;
   height:62px;
   &:hover {
+    cursor:pointer;
     background-color: #45622D;
-    border-color: #45622D;
+    border: none;
   }
 `;
 export const Input = styled.input`
   background: #FFFFFF;
   width:398px;
-  height:62px;
+  height:63px;
+  text-align: center;
+  font-family: "minecraft";
+  font-size: 20px;
+  font-weight: 500;
+  line-height: 22px;
+  letter-spacing: 0.075em;
+  color:#6D9948;
+  ::placeholder {
+    color: #BEB8B8;
+    font-family: "minecraft";
+    font-size: 20px;
+    font-weight: 500;
+    line-height: 22px;
+    letter-spacing: 0.075em;
+    text-align: center;
+  }
+`;
+export const Organizar = styled.div`
+   
+`;
+export const Item = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin-top: 85px;
+  margin-left: 70px;
 `;

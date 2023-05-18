4355942e6a7f108e1d9f5d555661f5f00973cc23
marco-stalin <marcosena@cpejr.com.br>
2023-05-18 18:18:28 -0300

    mais config da editar

--- src/pages/editar/editar.jsx
+++ src/pages/editar/editar.jsx
@@ -1,11 +1,24 @@
 //import { text } from "stream/consumers";
-import {Container, Title} from "./styles"
+import {Container, Title, Texto, MeuBotao, Input} from "./styles"
 
 export default function Editar(){
    return(
    <Container>
       <Title>O que deseja editar?</Title>
-      
+      <div>
+      <div>
+      <Texto>Nome: </Texto> <Input>
+         </Input><MeuBotao><Texto>Editar! </Texto></MeuBotao>
+      </div><br></br>
+      <div>
+        <Texto>Cargo: </Texto><Input>
+         </Input><MeuBotao><Texto>Editar! </Texto></MeuBotao>
+      </div><br></br>
+      <div>
+      <Texto>Status: </Texto><Input>
+         </Input><MeuBotao><Texto>Editar! </Texto></MeuBotao>
+      </div><br></br>
+   </div>
      </Container>
    );
 }
--- src/pages/editar/styles.js
+++ src/pages/editar/styles.js
@@ -2,21 +2,41 @@ import styled from "styled-components";
 import fundoMinecraft from "../../assets/editar/fundoMinecraft.jpg"
 
 export const Container = styled.div`
-background-position: center;
-  //background-repeat: no-repeat;
-  height: 100vh;
-  width: 100vw;
-  display:flex;
-  flex-direction: column;
-  overflow: auto;
-background-image: linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.5)),
-    url(${fundoMinecraft});
- // background-size: cover;
+  position:fixed;
+  padding:0;
+  margin:0;
+  top:0;
+  left:0;
+  width: 100%;
+  height: 100%;
+  background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.5)),
+  url(${fundoMinecraft});
+  background-size: cover;
 `;
-
 export const Title = styled.p`
-font-size: 30px;
-font-family: "minecraft";
-color: #FFFFFF;
-text-align: center;
+  font-size: 50px;
+  font-family: "minecraft";
+  color: #FFFFFF;
+  text-align: center;
+`;
+export const Texto = styled.span` 
+  font-size: 30px;
+  font-family: "minecraft";
+  color: #FFFFFF;
+  text-align: left;
+`;
+export const MeuBotao = styled.button`
+  background: #6D9948;
+  border-color: #6D9948;
+  width:215px;
+  height:62px;
+  &:hover {
+    background-color: #45622D;
+    border-color: #45622D;
+  }
+`;
+export const Input = styled.input`
+  background: #FFFFFF;
+  width:398px;
+  height:62px;
 `;

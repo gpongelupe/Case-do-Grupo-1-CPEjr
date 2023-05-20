9320b7c906983c8dfd12c661a56f899fa2a8e456
Ludmila Carvalho <ludmilamsaid@gmail.com>
2023-05-20 12:27:30 -0300

    Algumas configs da home

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,13 +1,25 @@
-import {Container, Title,BotaoLogin,Input} from "./styles"
-
+import {Container, Title,BotaoLogin,Input, Tabela,Texto, DivLogin,Carrosel} from "./styles"
+import { useState,useEffect } from "react";
 export default function Home(){
+
+   const {membros,setMembros}=useState([]);
    return(
    <Container>
       <Title></Title>
-      <div>
-      <Input placeholder="Nome do usuário"></Input>
-      <BotaoLogin>Login</BotaoLogin>
-      </div>
+      <Carrosel></Carrosel>
+      <DivLogin>
+      <Input placeholder="Nome do membro"></Input>
+      <BotaoLogin onClick ={()=> alert("Login realizado com sucesso!")}>
+        
+         Login
+      </BotaoLogin>
+      </DivLogin>
+
+      <Tabela>
+         <Texto>MEMBRO</Texto>
+         <Texto>CARGO</Texto>
+         <Texto>TEMPO</Texto>
+      </Tabela>
       
      </Container>
    );
